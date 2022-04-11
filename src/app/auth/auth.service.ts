ed4784c1ef5c0b1bd0ef781136312a8291d2c2fa
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-04-11 22:53:41 +0300

    added logic to check if user is authenticated

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,12 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { AuthData } from './auth-data.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private token: string;
+  private authStatusListener = new Subject<boolean>();
+
   constructor(private HttpClient: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = {
@@ -26,11 +38,13 @@ export class AuthService {
       email,
       password,
     };
-    this.HttpClient.post(
+    this.HttpClient.post<{ token: string }>(
       'http://localhost:3000/api/user/login',
       authData
     ).subscribe((response) => {
-      console.log(response);
+      const token = response.token;
+      this.token = token;
+      this.authStatusListener.next(true);
     });
   }
 }
