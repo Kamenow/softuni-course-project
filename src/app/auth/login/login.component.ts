7b679874c885aa569976eb30e5a06ffdca94d83e
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-04-11 13:06:43 +0300

    added login input fields

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'selector-name',
+  templateUrl: 'login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  isLoading: boolean = false;
+}
