6f9e59beed7fe526a8eb281729c354b9b2c9e5ea
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-04-12 14:48:44 +0300

    saving token in local storage

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,6 +14,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.userIsAuthenticated = this.authService.getIsAuth();
+
     this.authListenerSubs = this.authService
       .getAuthStatusListener()
       .subscribe((isAuthenticated) => {
